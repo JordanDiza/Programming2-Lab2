b926474ae843f6349ffe8369da23bb034789b04e
JordanDiza <dizaj8@gmail.com>
2026-09-08 15:47:41 -0400

    Did Lab2

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,39 +1,115 @@
 package org.example;
 
+// Part C
+
 public class Student {
     private String id;
     private String name;
     private double average;
 
-    public Student (String id, String name, double average)
-    {
+    public static void main(String[] args) {
+
+        Student student = new Student("2532424", "Drake", 98);
+        System.out.println("ID: " + student.getId());
+        System.out.println("Name: " + student.getName());
+        System.out.println("Average: " + student.getAverage());
+        // student1.setAverage(101.0);
+
+        Student[] students = {
+                new Student("V100001", "Amina", 86.5),
+                new Student("V100002", "Daniel", 72.0),
+                new Student("V100003", "Sofia", 91.0)};
+
+
+        for (Student studentList : students) {
+        System.out.println(studentList);
+        }
+
+        System.out.println(calculateAverage(students));
+
+
+        System.out.println(findStudent(students, "V100002"));
+
+
+
+    }
+
+    public Student(String id, String name, double average) {
         this.id = id;
         this.name = name;
-        this.average = average;
+
+        // Part D
+        if (average < 0.0 || average > 100.0) {
+            throw new Error("Average must be between 0 and 100");
+        } else {
+            this.average = average;
+        }
     }
 
-    public String getId(){
+    public String getId() {
+
         return id;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public double getAverage(){
+
+    public double getAverage() {
         return average;
     }
-    public void setAverage(double average){
-        this.average = average;
 
+    public void setAverage(double average) {
+        // Part D
+        if (average < 0.0 || average > 100.0) {
+            throw new Error("Average must be between 0 and 100");
+        } else {
+            this.average = average;
+        }
     }
 
     @Override
-    public String toString(){
+    public String toString() {
+        return "Student ID:" + id + " " + "Student Name:" + name + " " + "Average:" + average;
+    }
+
+    //Part E
+    public static double calculateAverage(Student[] students) {
+
+        double finalAverage = 0;
+        double calculatedAverage = 0;
+
+        for (Student studentAvg : students) {
+            finalAverage = finalAverage + studentAvg.getAverage();
+
+        }
+        calculatedAverage = finalAverage / students.length;
+        return calculatedAverage;
+    }
+
+    public static Student findStudent(Student[] students, String id) {
+
+        for (Student student : students) {
+            if (student.getId().equals(id)) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    // Part F
+    /*
+    public static Student findStudent(ArrayList<Student> students, String id){
 
     }
 
-    public static void main(String [] args){
+    public static int countAbove(ArrayList<Student> students, double threshold){
 
     }
-
+    */
 }
+
+
+
+
 
